385d4692069e7972f269e22321ec8c3a6c0682f8
XenoWars <43147270+XenoWars@users.noreply.github.com>
2020-07-02 11:58:28 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -14,7 +14,18 @@ app.use(function(req, res, next) {
 })
 if( app.thing == null ) console.log( 'bleat!' );
 
-app.get('/', function(req, res) {
+{
+	course1: {
+		code: 'CMPS 330', name: 'Electronic Commerce 1', description: 'This course provides information and analysis of e-commerce. Course topics include – e-commerce business models, ecommerce infrastructure, implementing an e-commerce website, e-commerce security and payment systems, ecommerce marketing concepts, ethical, social and political e-commerce issues, social networks, and how highly portable, place-aware, always-with-you personal devices are expanding the e-commerce environment. (No pre-requisites.) ', credits: '3', instructor: 'Andrea Wachter'
+	},
+	course2: {
+		code: 'CMPS 361', name: 'Web Application Development', dexcription: 'This course will provide a foundation in several facets of establishing and maintaining a website. This includes the latest advances in client side as well as server side technologies. The goal is to have students design, implement, and run advanced web applications. It will also cover in some detail the protocols required for web development. Prerequisites: CMPS 261, CMPS 262.', credits: '3', instructor: 'Mark Voortman'
+	},
+	course3: {
+		code: 'CMPS 364', name: 'NoSQL Databases', description: 'This course explores modern databases that do not strictly follow the relational database design and SQL. Advantages and disadvantages will be discussed. Students will be required to work with at least one NoSQL databases and write a program that utilizes it.', credits: '3', instructor: 'Jeffrey Seaman'
+	}
+}
+	app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req, res) {
