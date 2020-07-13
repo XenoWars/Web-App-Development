a0befe2fc82b29775861409628dca2100e5cdccf
XenoWars <43147270+XenoWars@users.noreply.github.com>
2020-07-13 12:43:17 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -14,29 +14,39 @@ app.use(function(req, res, next) {
 })
 if( app.thing == null ) console.log( 'bleat!' );
 
-//{
-//	course1: {
-//		code: "CMPS 330",
-//		name: "Electronic Commerce 1",
-//		description: "This course provides information and analysis of e-commerce. Course topics include – e-commerce business models, ecommerce infrastructure, implementing an e-commerce website, e-commerce security and payment systems, ecommerce marketing concepts, ethical, social and political e-commerce issues, social networks, and how highly portable, place-aware, always-with-you personal devices are expanding the e-commerce environment. (No pre-requisites.)",
-//		credits: "3",
-//		instructor: "Andrea Wachter"
-//	},
-//	course2: {
-//		code: "CMPS 361",
-//		name: "Web Application Development",
-//		dexcription: "This course will provide a foundation in several facets of establishing and maintaining a website. This includes the latest advances in client side as well as server side technologies. The goal is to have students design, implement, and run advanced web applications. It will also cover in some detail the protocols required for web development. Prerequisites: CMPS 261, CMPS 262.",
-//		credits: "3",
-//		instructor: "Mark Voortman"
-//	},
-//	course3: {
-//		code: "CMPS 364",
-//		name: "NoSQL Databases",
-//		description: "This course explores modern databases that do not strictly follow the relational database design and SQL. Advantages and disadvantages will be discussed. Students will be required to work with at least one NoSQL databases and write a program that utilizes it.",
-//		credits: "3",
-//		instructor: "Jeffrey Seaman"
-//	}
-//}
+app.use(require('body-parser')());
+app.post('/process', function(req, res){
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Form (from querystring): ' + req.query.form);
+	console.log('Name token (from hidden form field): ' + req.body._csrf);
+	console.log('Email (from visible form field): ' + req.body.name);
+	console.log('Comment (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you')
+});
+
+var courseInfo = [
+	{
+		"code": "CMPS 330",
+		"name": "Electronic Commerce 1",
+		"description": "This course provides information and analysis of e-commerce. Course topics include – e-commerce business models, ecommerce infrastructure, implementing an e-commerce website, e-commerce security and payment systems, ecommerce marketing concepts, ethical, social and political e-commerce issues, social networks, and how highly portable, place-aware, always-with-you personal devices are expanding the e-commerce environment. (No pre-requisites.)",
+		"credits": "3",
+		"instructor": "Andrea Wachter"
+	},
+	{
+		"code": "CMPS 361",
+		"name": "Web Application Development",
+		"description": "This course will provide a foundation in several facets of establishing and maintaining a website. This includes the latest advances in client side as well as server side technologies. The goal is to have students design, implement, and run advanced web applications. It will also cover in some detail the protocols required for web development. Prerequisites: CMPS 261, CMPS 262.",
+		"credits": "3",
+		"instructor": "Mark Voortman"
+	},
+	{
+		"code": "CMPS 364",
+		"name": "NoSQL Databases",
+		"description": "This course explores modern databases that do not strictly follow the relational database design and SQL. Advantages and disadvantages will be discussed. Students will be required to work with at least one NoSQL databases and write a program that utilizes it.",
+		"credits": "3",
+		"instructor": "Jeffrey Seaman"
+	}
+];
 	app.get('/', function(req, res) {
 	res.render('home');
 });
@@ -48,8 +58,11 @@ app.get('/about', function(req, res) {
 app.get('/courses', function(req, res) {
 	res.render('courses');
 });
+app.get('/process', function(req, res) {
+	res.render('process');
+});
 app.get('/contact', function(req, res) {
-	res.render('contact');
+	res.render('contact', { csrf: 'CSRF token goes here' });
 });
 app.get('/games', function(req, res) {
 	res.render('games', {
